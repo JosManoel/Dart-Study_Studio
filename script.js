1c8b28b48a36cc5b13113f99e531ffaed57eb0e3
JosManoel <josmanoelfreitas2012@gmail.com>
2022-01-18 16:23:45 -0300

    Version beta

--- script.js
+++ script.js
@@ -1,111 +1,75 @@
-// #################################################################
-// COMPARTILHAR OS INPUTS DO FORMULARIO COM O BANNER E PREVIEW
+const html = document.getElementsByTagName('html')[0];
 
-let inputBanner = document.getElementById( 'inputBanner');
-let inputTitle = document.getElementById('inputTitle');
-let inputShield = document.getElementById('inputShield');
+const previewBanner = document.getElementById('previewBanner');
+const previewShield = document.getElementById('previewShield');
+const previewTextWrapper = document.getElementById('previewTextWrapper');
 
-let previewBanner = document.getElementById('previewBanner');
-let previewTitle = document.getElementById('previewTitle');
-let previewShield = document.getElementById('previewShield');
+const renderBanner = document.getElementById('renderBanner');
+const renderShield = document.getElementById('renderShield');
+const renderTextWrapper = document.getElementById('renderTextWrapper');
 
-let renderBanner = document.getElementById('renderBanner');
-let renderTitle = document.getElementById('renderTitle');
-let renderShield = document.getElementById('renderShield');
+const inputBanner = document.getElementById( 'inputBanner');
+const inputBannerCount = document.getElementById( 'inputBannerCount');
+const inputShield = document.getElementById('inputShield');
+const inputShieldCount = document.getElementById('inputShieldCount');
+const inputPrimaryColor = document.getElementById('inputPrimaryColor');
+const inputSecondaryColor = document.getElementById('inputSecondaryColor');
+const inputTextBannerColor = document.getElementById('inputTextBannerColor');
 
+const primaryColorPicker = document.getElementById('primaryColorPicker');
+const secondaryColorPicker = document.getElementById('secondaryColorPicker');
+const textBannerColorPicker = document.getElementById('textBannerColorPicker');
+
+
+// # Tratamento de inputs de texto
 
 inputBanner.addEventListener('keyup', () => {
     renderBanner.innerHTML = previewBanner.innerHTML = inputBanner.value;
-});
+    inputBannerCount.innerHTML = $(inputBanner).val().length.toString() + "/64";
 
-inputTitle.addEventListener('keyup', () => {
-    renderTitle.innerHTML = previewTitle.innerHTML = inputTitle.value;
+    resizeTextBanner(previewBanner, inputBanner, 18, previewTextWrapper);
+    resizeTextBanner(renderBanner, inputBanner, 44, renderTextWrapper);
 });
 
 inputShield.addEventListener('keyup', () => {
     renderShield.innerHTML = previewShield.innerHTML = inputShield.value;
+    inputShieldCount.innerHTML = $(inputShield).val().length.toString() + "/32";
 });
 
-// ##################################################################
-// BAIXAR ELEMENTO SELECIONADO
-// o elemento e o nome sao passados no metodo onclick()
-
-function downloadImage(element, nameImage = "image"){
-    let image = document.querySelector(element);
-
-    html2canvas(image, {
-        backgroundColor: null
-    }).then(canvas => {
-        let a = document.createElement("a");
-
-        a.href = canvas.toDataURL('image/png');
-        a.download =  nameImage;
-        a.click();
-    });
-}
-
-// ##################################################################
-// MODELAR O BANNER PREVIEW PROPORCIONALMENTE AO RENDER
+// # Redimensionando o preview para a proporção correta
 
 function resizeBannerPreview(){
     let bannerPreview = document.querySelector('.preview-banner__background');
     $(bannerPreview).height(bannerPreview.clientWidth / 5);
 }
 
-// ###################################################################
-// MUDANÇA DE TEMA
+// # Redimensionamento automatico do texto no banner 
 
-// SELECIONA O TEMA DE ACORDO COM RADIOBUTTOM
-
-let darkDimmedSwitch = document.querySelector('#darkDimmed');
-darkDimmedSwitch.addEventListener('click', () => {
-    setThemeMode('dark-dimmed');
-});
-
-let defaultDarkSwitch = document.querySelector('#defaultDark');
-defaultDarkSwitch.addEventListener('click', () => {
-    setThemeMode('default-dark');   
-});
-
-let defaultLightSwitch = document.querySelector('#defaltLight');
-defaultLightSwitch.addEventListener('click', () => {
-   setThemeMode('default-light');
-});
-
-
-// MUDA O TEMA E SALVA NO LOCAL STORAGE
-function setThemeMode(themeMode){
-    let page = document.getElementsByTagName('html');
-
-    $(page).removeClass();
-    $(page).addClass(themeMode);
-
-    localStorage.setItem('themeMode', themeMode);
-}
-
-// VERIFICA SE EXISTE UM TEMA INICIAL E ATIVA-O 
-function getThemeMode(){
-    const themeMode = localStorage.getItem('themeMode');
-    setThemeMode(themeMode);
-
-    switch (themeMode) {
-        case 'default-light':
-            defaultLightSwitch.checked = true;
-            break;
-
-        case 'default-dark':
-            defaultDarkSwitch.checked = true;
-            break;
-        
-        case 'dark-dimmed':
-            darkDimmedSwitch.checked = true;
-            break;
+function resizeTextBanner(text, inputText, minSize, wrapper){
+    var textLenght = $(inputText).val().length;
+    var wrapperWidth = parseInt($(wrapper).css('width').replace("px",""));
+    var wrapperHeight = parseInt($(wrapper).css('height').replace("px",""));
+    
+    var fontSize = (wrapperWidth * Math.sqrt(2))/textLenght;
+    
+    if (fontSize >= wrapperHeight/ (Math.sqrt(2))) {
+        text.style.fontSize = maxTextSize(wrapperHeight);
+    } else if (fontSize < minSize){
+        text.style.fontSize = minTextSize(minSize);
+    }else{
+        text.style.fontSize = (fontSize).toString() + "px";
     }
 }
 
-// ###################################################################
-// ABRE UMA IMAGEM E ADICIONA AO PREVIEW E AO RENDER, AO MESMO TEMPO
-// QUE MUDA O TEXTO DO INPUT FILE CUSTOMIZADO.
+function maxTextSize(wrapperHeight){
+    return (wrapperHeight * 0.75).toString() + "px";
+}
+
+function minTextSize(minSize){
+    return ((minSize).toString() + "px");
+}
+
+// # Adicionando uma imagem ao banner
 
 $(function() {
     $('#upload').change(function(){
@@ -124,21 +88,135 @@ $(function() {
     });
 });
 
-// ###################################################################
+// # Configuração de cores do banner
+
+function setColorValue(string, colorValue, inputColor){
+    html.style.setProperty(string, $(colorValue).val());
+    inputColor.value = $(colorValue).val().replace("#","");
+}
+
+function changeColor(element, colorValue){
+    html.style.setProperty(element, "#" + colorValue);
+}
+
+// cor primaria
+
+$(primaryColorPicker).on('change', function () {  
+    setColorValue("--banner-primary-color", this, inputPrimaryColor);
+});
+$(inputPrimaryColor).on('keypress', function(e){
+    if(e.key === 'Enter'){
+        changeColor("--banner-primary-color", inputPrimaryColor.value);
+    }
+});
+
+// cor secundaria
+
+$(secondaryColorPicker).on('change', function () {  
+    setColorValue("--banner-secondary-color", this, inputSecondaryColor);
+});
+$(inputSecondaryColor).on('keypress', function(e){
+    if(e.key === "Enter"){
+        changeColor("--banner-secondary-color", inputSecondaryColor.value);
+    }
+});
+
+// cor do texto
+
+$(textBannerColorPicker).on('change', function () {
+    setColorValue("--color-banner-text", this, inputTextBannerColor);
+});
+$(inputTextBannerColor).on('keypress', function(e){
+    if(e.key === "Enter"){
+        changeColor("--color-banner-text", inputTextBannerColor.value);
+    }
+});
+
+// input de cor hexadecimal
+
+$(document).on('click', function(e) {
+    if(!inputPrimaryColor.contains(e.target) && (inputPrimaryColor.value != "")){
+        changeColor("--banner-primary-color", inputPrimaryColor.value);
+    }
+    
+    if(!inputSecondaryColor.contains(e.target) && (inputSecondaryColor.value != "")){
+        changeColor("--banner-secondary-color", inputSecondaryColor.value);
+    }
+
+    if(!inputTextBannerColor.contains(e.target) && (inputTextBannerColor.value != "")){
+        changeColor("--color-banner-text", inputTextBannerColor.value);
+    }
+});
+
+// # Download da imagem gerada
+
+function downloadImage(element, nameImage = "image"){
+    let image = document.querySelector(element);
+
+    html2canvas(image, {
+        backgroundColor: null
+    }).then(canvas => {
+        let a = document.createElement("a");
+
+        a.href = canvas.toDataURL('image/png');
+        a.download =  nameImage;
+        a.click();
+    });
+}
 
 
+// # Seleção de temas
 
+let darkDimmedSwitch = document.querySelector('#darkDimmed');
+darkDimmedSwitch.addEventListener('click', () => {
+    setThemeMode('dark-dimmed');
+});
 
+let defaultDarkSwitch = document.querySelector('#defaultDark');
+defaultDarkSwitch.addEventListener('click', () => {
+    setThemeMode('default-dark');   
+});
 
+let defaultLightSwitch = document.querySelector('#defaltLight');
+defaultLightSwitch.addEventListener('click', () => {
+   setThemeMode('default-light');
+});
 
+// armazenamento do estado de tema atual
 
+function setThemeMode(themeMode){
+    let page = document.getElementsByTagName('html');
 
+    $(page).removeClass();
+    $(page).addClass(themeMode);
 
+    localStorage.setItem('themeMode', themeMode);
+}
 
+// recuperando o estado de tema anterior
 
-// ##################################################################
+function getThemeMode(){
+    const themeMode = localStorage.getItem('themeMode');
+    var theme = themeMode != null ? themeMode : 'dark-dimmed';
 
+    setThemeMode(theme);
 
+    switch (themeMode) {
+        case 'default-light':
+            defaultLightSwitch.checked = true;
+            break;
+
+        case 'default-dark':
+            defaultDarkSwitch.checked = true;
+            break;
+        
+        case 'dark-dimmed':
+            darkDimmedSwitch.checked = true;
+            break;
+    }
+}
+
+// # Funções iniciais
 
 $(document).ready( () => {
     getThemeMode();
