a47e43645f485ffbc434251daf23ca5bb141df65
JosManoel <josmanoel.freitas@gmail.com>
2022-09-25 18:29:23 -0300

    :tada: new assets :tada:

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+const white = Color(0xFFFFFFFF);
+const blackHaze = Color(0xFFF6F8FA);
+const gallery = Color(0xFFEDEDEE);
+const alto = Color(0xFFDADADA);
+const grayChateau = Color(0xFF9DA7B3);
+const casper = Color(0xFFA9BAC7);
+const riverBed = Color(0xFF444C56);
+const tuna = Color(0xFF2D333B);
+const shark = Color(0xFF22272E);
+const sharkBlack = Color(0xFF1C2128);
+const cornflowerBlue = Color(0xFF539BF5);
+const marine = Color(0xFF316DCA);
+
